1307fad9245d988a389491e06db3734b3ba284e1
Juan David Paniagua Cano <david0613pc@gmail.com>
2022-08-09 16:52:17 -0500

    commit-09_08_2022
    backend con node modified

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require('express');
-
 const { routerApi } = require('./routes/index.js');
-
 const { logErrors, errorHandler, boomErrorHandler  } = require('./middlewares/errorHandler.js');
 const cors = require('cors')
-
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
+//*CORS 
 //*Le da acceso a todo el mundo
 // app.use(cors());
-
 //*Asi se da acceso solo a los que se supone que se contemplan
 const whiteList = ['http://localhost:8080'] //*Tambien podrian ponerse dominios
 const options = {
@@ -24,14 +21,13 @@ const options = {
     }
   }
 }
-
 app.use(cors(options));
 
 
 //*Acceso a la capa de routing
 routerApi(app);
 
-//*Uso de middlewares de errores, el orden es importante.
+//*Uso de middlewares de errores, deben ir despues del routing.
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
@@ -41,14 +37,9 @@ app.listen(port, () => {
 });
 
 
-
-
-//*ROUTING CON EXPRESS:
-//*FUNCIONAMENTO A PROFUNDIDAD DE GET.
-
 //*Recibir parametros, en el requets solemos tener un objeto que se llama params que contiene todo lo que hemos agreado de parámetros. Usar destructuración para sacar los parámetros. Los endpoints que son espesificos deben ir antes de los que son dinamicos.
   app.get('/', (req, res) => {
-    res.send('Respuesta del servidor a la ruta por defecto');
+    res.send('Acabas de hacer una petición a la raiz del servidor');
   });
 
   // app.get('/home', (req, res) => {
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,18 +1,15 @@
 const express = require('express');
-
 const {UsersService} = require('../services/usersService.js');
-
 const usersRouter = express.Router();
 
 // const {validatorHandler} = require('../middlewares/validatorHandler.js');
 // const {createUserSchema,getUserSchema,updateUserSchema} = require('../schemas/usersSchemas.js')
 
-const usuarios = new UsersService();
-
+const userService = new UsersService();
 
 usersRouter.get('/', async (req,res,next) => {
   try{
-    const users = await usuarios.find()
+    const users = await userService.find();
     res.json(users)
   }
   catch(error){
@@ -20,11 +17,30 @@ usersRouter.get('/', async (req,res,next) => {
   }
 });
 
+//*Ejemplo con promesas 
+(()=>{
+// usersRouter.get('/',  (req,res,next) => {
+//   return new Promise((resolve,reject)=>{
+//     const users = userService.find();
+//     if(users){
+//       resolve(users);
+//     }
+//     else{
+//       reject(users)
+//     }
+//   })
+//   .then(users => res.json(users))
+//   .catch(error => next(error))
+// });
+})
+
+
+
 usersRouter.get('/:guid',
 async (req, res, next) => {
   try{
       const { guid } = req.params;
-      const user = await usuarios.findOne(guid);
+      const user = await userService.findOne(guid);
       res.json(user);
   } catch(error){
     //*Esto lo  que hace es llamar a los middlewares de tipo error.
@@ -34,7 +50,7 @@ async (req, res, next) => {
 
 usersRouter.post('/',async (req,res)=>{
   const body = req.body;
-  const newUser = await usuarios.create(body);
+  const newUser = await userService.create(body);
   res.status(201).json(newUser);
 })
 
@@ -43,7 +59,7 @@ async (req, res, next) => {
   try {
   const {guid} = req.params;
   const body = req.body
-  const updatedUser = await usuarios.update(guid,body)
+  const updatedUser = await userService.update(guid,body)
   res.json(updatedUser);
   }
   catch(error){
@@ -54,7 +70,7 @@ async (req, res, next) => {
 usersRouter.delete('/:guid',
 async (req, res) => {
   const { guid } = req.params;
-  const deletedUser = await usuarios.delete(guid)
+  const deletedUser = await userService.delete(guid)
   res.json(deletedUser);
 });
 
